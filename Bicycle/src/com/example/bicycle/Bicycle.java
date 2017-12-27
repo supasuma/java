3c3b4ab8702278832f530a0955468575f3bad065
supasuma <summer.brines@babylonhealth.com>
2017-12-27 16:41:47 +0000

    I want to ride my bicycle

---
+++ Bicycle/src/com/example/bicycle/Bicycle.java
@@ -0,0 +1,27 @@
+package com.example.bicycle;
+
+public class Bicycle {
+    private int cadence = 0;
+    private int speed = 0;
+    private int gear = 1;
+
+    public void changeCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public void changeGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void speedUp(int increment) {
+        speed = speed + increment;
+    }
+
+    public void applyBrakes(int decrement) {
+        speed = speed - decrement;
+    }
+
+    public void printStates() {
+        System.out.println("cadence:" + cadence + " speed:" + speed + " gear:" + gear);
+    }
+}
