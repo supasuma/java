ad6e2f6a53ff6e4ba974b50c491ea34b6c54f315
supasuma <summer.brines@babylonhealth.com>
2017-12-27 13:21:09 +0000

    testing git integration

--- HelloWorld/src/com/example/helloworld/HelloWorld.java
+++ HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("it's me, Wombat!");
+        System.out.println("trying out git integration");
     }
 }
