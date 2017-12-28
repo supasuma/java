7d6d31dfec8d80492fa6249379e8eaca4c67b9d8
supasuma <summer.brines@babylonhealth.com>
2017-12-28 15:38:22 +0000

    playing with array methods

--- Bicycle/src/com/practice/arraydemo/ArrayCopyDemo.java
+++ Bicycle/src/com/practice/arraydemo/ArrayCopyDemo.java
@@ -1,5 +1,7 @@
 package com.practice.arraydemo;
 
+import java.util.Arrays;
+
 public class ArrayCopyDemo {
     public static void main(String[] args) {
         char[] copyFrom = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
@@ -9,5 +11,22 @@ public class ArrayCopyDemo {
 
 //        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
         System.out.println(new String(copyTo));
+
+        String thingsStuff;
+        System.out.println(thingsStuff);
+
+
+        int some = Arrays.binarySearch(copyFrom, 'i');
+        System.out.println(some);
+
+        boolean outcome = java.util.Arrays.equals(copyFrom, copyTo);
+        System.out.println(outcome);
+
+        char[] sortArray = { 'd', 'e', 'c', 'a', 'f', 'f', 'e',
+                'i', 'n', 'a', 't', 'e', 'd' };
+
+//        Arrays.sort(sortArray);
+        Arrays.parallelSort(sortArray);
+        System.out.println(sortArray);
     }
 }
