dfd56c1adbcaf681ddc305132adcf11b957f104a
supasuma <summer.brines@babylonhealth.com>
2017-12-29 15:52:01 +0000

    fixing mountain bike subclass

--- Bicycle/src/com/example/mountainbike/MountainBike.java
+++ Bicycle/src/com/example/mountainbike/MountainBike.java
@@ -3,4 +3,7 @@ package com.example.mountainbike;
 import com.example.awesomebicycle.AwesomeBicycle;
 
 public class MountainBike extends AwesomeBicycle {
+    public MountainBike(int startCadence, int startSpeed, int startGear) {
+       super(startCadence, startSpeed, startGear);
+    }
 }
