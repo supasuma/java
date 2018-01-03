111bc7b44ad20baca41fc91d0292890d1b43e2d6
supasuma <summer.brines@babylonhealth.com>
2018-01-03 11:45:08 +0000

    Add methods to return values and no argument constructor

--- Bicycle/src/com/example/awesomebicycle/AwesomeBicycle.java
+++ Bicycle/src/com/example/awesomebicycle/AwesomeBicycle.java
@@ -1,34 +1,55 @@
 package com.example.awesomebicycle;
 
-
 import com.example.bicycle.Bicycle;
 
 public class AwesomeBicycle implements Bicycle {
+    // three fields (member variables)
     private int cadence;
     private int speed;
     private int gear;
 
+    //one  constructor
     public AwesomeBicycle(int startCadence, int startSpeed, int startGear) {
         gear = startGear;
         cadence = startCadence;
         speed = startSpeed;
     }
 
+    // can also have a no argument constructor because they have diff argument lists
+    public AwesomeBicycle() {
+        gear = 1;
+        cadence = 10;
+        speed = 0;
+    }
+
     public void changeCadence(int newValue) {
+        // variables in a method are local variables
         cadence = newValue;
+    }
+
+    public int getCadence() {
+        return cadence;
     }
 
     public void changeGear(int newValue) {
         gear = newValue;
     }
 
+    public int getGear() {
+        return gear;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
     public void speedUp(int increment) {
-        speed = speed + increment;
+        speed += increment;
     }
 
     public void applyBrakes(int decrement) {
         if (isMoving()){
-            speed = speed - decrement;
+            speed -= decrement;
         } else {
             System.out.println("bike has already stopped!");
         }
--- Bicycle/src/com/example/mountainbike/MountainBike.java
+++ Bicycle/src/com/example/mountainbike/MountainBike.java
@@ -3,7 +3,15 @@ package com.example.mountainbike;
 import com.example.awesomebicycle.AwesomeBicycle;
 
 public class MountainBike extends AwesomeBicycle {
-    public MountainBike(int startCadence, int startSpeed, int startGear) {
+
+    public int seatHeight;
+
+    public MountainBike(int startHeight, int startCadence, int startSpeed, int startGear) {
        super(startCadence, startSpeed, startGear);
+       seatHeight = startHeight;
+    }
+
+    public void setHeight(int newValue) {
+        seatHeight = newValue;
     }
 }
